1e0376adc81fccf72636a30247b10b1cecbda2b2
David A. <me@myself.i>
2024-12-29 17:20:44 +0100

    wip

--- vars/remote_deploy.groovy
+++ vars/remote_deploy.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String DEPLOY_ENV="", String CREDID="", String IMAGE_NAME="", String IMAGE_TAG="", String DB_HOST="", String DB_USER="", String DB_PASS="") {
-    withCredentials([usernamePassword(credentialsId: '$CREDID', usernameVariable: 'DOCKERHUB_CREDENTIALS_USR', passwordVariable: 'DOCKERHUB_CREDENTIALS_PWD')]) {
+    withCredentials([usernamePassword(credentialsId: ${CREDID}, usernameVariable: 'DOCKERHUB_CREDENTIALS_USR', passwordVariable: 'DOCKERHUB_CREDENTIALS_PWD')]) {
         sh '''
         [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
         ssh-keyscan -t rsa,dsa,ed25519 ${DEPLOY_ENV} >> ~/.ssh/known_hosts
